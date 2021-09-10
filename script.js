17b85b319d0203882ff92f884e1ac8ad923e2b84
Emily <eberge11@Jhu.edu>
2021-09-09 23:47:56 -0400

    Header started

---
+++ script.js
@@ -0,0 +1,11 @@
+function goGithub() {
+  window.location.href = "https://github.com/heyitsM"
+}
+
+function goLinkedin() {
+  window.location.href = "https://www.linkedin.com/in/emily-berger-941161207/"
+}
+
+function goEmail() {
+  window.location.href = "mailto:eberge11@jhu.edu"
+}
